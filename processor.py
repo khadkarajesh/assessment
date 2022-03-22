f14d556408c6dd4533b47b8ec8dcd1e59e8684ce
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-03-22 07:58:04 +0100

    change private scope to public in method name

--- processor.py
+++ processor.py
@@ -8,7 +8,7 @@ LOG_FORMAT_REGEX = r"^[0-9]+\|[0-9]+\|[A-Z]{2}$"
 OUTPUT_FILE_NAME_PREFIX = "country_top50_"
 
 
-def _count_frequency_of_songs_by_country(file) -> dict:
+def count_frequency_of_songs_by_country(file) -> dict:
     country_songs = {}
     for line in file:
         pattern = re.compile(LOG_FORMAT_REGEX)
@@ -27,12 +27,12 @@ def _count_frequency_of_songs_by_country(file) -> dict:
     return country_songs
 
 
-def _save(values: List[str], file_name: str, output_path: Path) -> None:
-    with open(output_path / _get_filename(file_name), "w") as result_file:
+def save(values: List[str], file_name: str, output_path: Path) -> None:
+    with open(output_path / get_filename(file_name), "w") as result_file:
         result_file.writelines(values)
 
 
-def _get_filename(input_file_name: str) -> str:
+def get_filename(input_file_name: str) -> str:
     log_file_date = input_file_name[7:17]
     return f"{OUTPUT_FILE_NAME_PREFIX}{log_file_date}.txt"
 
@@ -41,7 +41,7 @@ def sort_by_value(data: dict, reverse: bool = True) -> dict:
     return {k: v for k, v in sorted(data.items(), key=lambda item: item[1], reverse=reverse)}
 
 
-def _filter_top_n_songs(n: int, count_dict: dict) -> List[str]:
+def filter_top_n_songs(n: int, count_dict: dict) -> List[str]:
     result = []
     for key in count_dict.keys():
         songs_dict = count_dict.get(key)
@@ -60,8 +60,8 @@ def get_top_songs(n: int,
                   input_file) -> None:
     try:
         with open(data_path / 'input' / input_file) as f:
-            data = _count_frequency_of_songs_by_country(f)
-            top_n_songs = _filter_top_n_songs(n=n, count_dict=data)
-            _save(top_n_songs, input_file, output_path=output_path)
+            data = count_frequency_of_songs_by_country(f)
+            top_n_songs = filter_top_n_songs(n=n, count_dict=data)
+            save(top_n_songs, input_file, output_path=output_path)
     except FileNotFoundError as e:
         logger.error(e)
