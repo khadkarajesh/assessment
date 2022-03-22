66da5dabb6a262bf0f7a66cfab7a84a41eb8a042
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-03-22 12:00:09 +0100

    add command line arguments

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import argparse
 import concurrent.futures
 import datetime
 import time
@@ -25,6 +26,14 @@ def user_song_wrapper(p):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Description",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-t", "--type", default="country_top_50_song", help="generate type of report")
+    args = parser.parse_args()
+    config = vars(args)
+
+    action_type = config.get('type')
+
     print("Job started....")
     print("Job in progress....")
 
@@ -35,11 +44,13 @@ if __name__ == "__main__":
     file_names = [f"listen-{date}.log" for date in dates]
 
     results = prepare_args(names=file_names)
-
-    # with concurrent.futures.ProcessPoolExecutor() as executor:
-    #     results = executor.map(country_song_wrapper, results)
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = executor.map(user_song_wrapper, results)
+    if action_type == "country_top_50_song":
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(country_song_wrapper, results)
+            results = executor.map(user_song_wrapper, results)
+    else:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(user_song_wrapper, results)
     elapsed_time = time.time() - start_time
 
     print(f"Job completed. Elapsed time {round(elapsed_time, 2)} sec")
