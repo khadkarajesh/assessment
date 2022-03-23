3cbe307b70dec5ec56ce067be39311ddbf9b25db
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-03-23 20:06:36 +0100

    remove commented code

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,3 @@
-import pytest
-
 from utils import sort_by_value
 
 
@@ -8,13 +6,3 @@ def test_sort_by_value_should_sort_dict_in_reverse_order():
     actual_dict = sort_by_value(test_dict)
     expected_dict = {'2': 21, '1': 10}
     assert actual_dict == expected_dict
-
-# @pytest.mark.usefixtures('filename')
-# def test_save_should_write_to_file_in_output_path_directory(tmp_path, filename, output_filename):
-#     directory = tmp_path / 'output'
-#     directory.mkdir()
-#     processor.save(values=["NP|103:10\n"],
-#                    file_name=filename,
-#                    output_path=directory)
-#     with open(directory / output_filename, "r") as file:
-#         assert file.readlines()[0] == "NP|103:10\n"
