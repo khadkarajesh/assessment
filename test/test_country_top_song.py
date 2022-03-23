edb7fa885fd0accef2ff02f7d14b1cbff2b7061d
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-03-23 20:03:07 +0100

    refactor test case

--- test/test_country_top_song.py
+++ test/test_country_top_song.py
@@ -5,10 +5,11 @@ def test_get_filename_should_return_outfile_name(country_top_song, output_filena
 def test_count_frequency_of_songs_by_country_should_return_songs_counts_as_by_country(country_top_song, input_file):
     expected_dict = {'NP': {'101': 1, '102': 1}}
     with open(input_file) as file:
-        country_top_song.count_frequency_of_songs_by_country(file)
-        assert country_top_song.country_songs == expected_dict
+        country_top_song.count_frequency(file)
+        assert country_top_song.count_storage == expected_dict
 
 
 def test_filter_top_n_songs_should_filter_songs_as_specified_in_number(country_top_song):
-    result = country_top_song.filter_top_n_songs(count_dict={'NP': {'101': 1, '102': 1, '103': 10}})
-    assert result[0] == "NP|103:10\n"
+    country_top_song.count_storage = {'NP': {'101': 1, '102': 1, '103': 10}}
+    country_top_song.filter()
+    assert country_top_song.result[0] == "NP|103:10\n"
--- test/test_user_top_song.py
+++ test/test_user_top_song.py
@@ -5,10 +5,11 @@ def test_get_filename_should_return_outfile_name(user_top_song, user_output_file
 def test_count_frequency_of_songs_by_user_should_count_songs_played_by_user(user_top_song, input_file):
     expected_dict = {'201': {'101': 1, '102': 1}}
     with open(input_file) as file:
-        user_top_song.count_frequency_of_songs_by_user(file)
-        assert user_top_song.user_songs == expected_dict
+        user_top_song.count_frequency(file)
+        assert user_top_song.count_storage == expected_dict
 
 
 def test_filter_top_n_songs_should_filter_songs_as_specified_in_number(user_top_song):
-    result = user_top_song.filter_top_n_songs(count_dict={'201': {'101': 1, '102': 1, '103': 10}})
-    assert result[0] == "201|103:10\n"
+    user_top_song.count_storage = {'201': {'101': 1, '102': 1, '103': 10}}
+    user_top_song.filter()
+    assert user_top_song.result[0] == "201|103:10\n"
